5398f774fc2a17a33773204fdc76bad2ffdd82cf
Alamgir <radhika.shankar@loram.com>
2022-03-10 14:42:06 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+const App = () => {
+  const [gifs, setGifs] = useState();
+  const [query, setQuery] = useState();
+  console.log('query', query);
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.giphy.com/v1/gifs/trending?api_key=Dst7UyI10lCaZeA9seXlAWA2qaXf0uGY'
+      )
+      .then((response) => {
+        console.log('response', response.data);
+        setGifs(response.data.data);
+      });
+  }, []);
+  console.log('gifs', gifs);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        placeholder='Search...'
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+      ></input>
+      <div style={{ border: 'solid', width: 1000 }}>
+        {gifs &&
+          gifs.map((gif) => (
+            <iframe
+              key='id'
+              title={gif.title}
+              src={gif.embed_url}
+              width='480'
+              height='270'
+              frameBorder='0'
+              className='giphy-embed'
+              allowFullScreen
+            >
+              {' '}
+            </iframe>
+          ))}
+      </div>
+    </>
   );
-}
-
+};
 export default App;
